b61824b7f1532b5692a1f11a307d07e2bb0110dc
Patricia Santos <patriciasanttos.dev@gmail.com>
2025-04-02 12:22:57 -0300

    responsiveness created

--- web/src/Components/ItemList/ItemList.jsx
+++ web/src/Components/ItemList/ItemList.jsx
@@ -1,34 +1,41 @@
 import "./ItemList.scss";
 import ItemImage from "../../assets/Images/itemImage.svg";
+import DeleteIcon from "../../assets/icons/deleteIcon.svg";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
-import deleteIcon from "../../assets/icons/deleteIcon.svg";
+import categoriesMock from "../../mocks/categoriesMocks.json";
+import QuantityInput from "./QuantityInput";
 
 function ItemList() {
-  const [productList, setProductList] = useState([
-    {
-      id: 1,
-      image: ItemImage,
-      name: "Kit Canetas",
-      value: 10.0,
-      quantity: 1,
-      subtotal: 10.0,
-    },
-    {
-      id: 2,
-      image: ItemImage,
-      name: "Kit Lápis",
-      value: 5.0,
-      quantity: 1,
-      subtotal: 5.0,
-    },
-  ]);
+  const isMobile = useMediaQuery({ maxWidth: 821 });
+
+  const getProductById = (id) => {
+    const products = categoriesMock.data.reduce(
+      (acc, category) => [...acc, ...category.items],
+      []
+    );
+    const results = products.filter((item) => item.id == id);
+
+    return results.length > 0
+      ? {
+          ...results[0],
+          quantity: 1,
+          subtotal: Number(results[0].price),
+        }
+      : null;
+  };
+
+  const cartCookie = JSON.parse(localStorage.getItem("cart")) || {};
+  const [productList, setProductList] = useState(
+    Object.keys(cartCookie).map(getProductById)
+  );
+  const [lastUpdatedIndex, setLastUpdatedIndex] = useState(null);
 
   const formatValues = (value) => {
-    const formattedValue = value.toFixed(2).replace(".", ",");
+    const formattedValue = Number(value).toFixed(2).replace(".", ",");
 
-    if (value < 10) {
+    if (Number(value) < 10) {
       return `R$0${formattedValue}`;
     }
 
@@ -44,25 +51,15 @@ function ItemList() {
     return total;
   };
 
-  const onChangeQuantity = (index, value) => {
-    const products = [...productList];
-
-    if (value >= 0) {
-      products[index].quantity = value;
-      products[index].subtotal =
-        products[index].value * products[index].quantity;
-      setProductList(products);
-    }
-  };
-
   const onClickLess = (index) => {
     const products = [...productList];
 
     if (products[index].quantity > 0) {
       products[index].quantity = products[index].quantity - 1;
       products[index].subtotal =
-        products[index].value * products[index].quantity;
+        products[index].price * products[index].quantity;
       setProductList(products);
+      setLastUpdatedIndex(index);
     }
   };
 
@@ -72,8 +69,9 @@ function ItemList() {
     if (products[index].quantity < 99) {
       products[index].quantity = products[index].quantity + 1;
       products[index].subtotal =
-        products[index].value * products[index].quantity;
+        products[index].price * products[index].quantity;
       setProductList(products);
+      setLastUpdatedIndex(index);
     }
   };
 
@@ -81,105 +79,153 @@ function ItemList() {
     const products = [...productList];
     products.splice(index, 1);
     setProductList(products);
+
+    // Remove from localstorage
+    const cartCookie = JSON.parse(localStorage.getItem("cart"));
+    delete cartCookie[productList[index].id];
+
+    if (Object.keys(cartCookie).length === 0) localStorage.removeItem("cart");
+    else localStorage.setItem("cart", JSON.stringify(cartCookie));
   };
 
   const removeAllItems = () => {
     setProductList([]);
+    localStorage.removeItem("cart");
+  };
+
+  const onChangeQuantity = (index, e) => {
+    e.preventDefault();
+
+    const quantity = Number(e.target.value);
+    const products = [...productList];
+
+    if (quantity >= 0 && quantity <= 99) {
+      products[index].quantity = quantity;
+      products[index].subtotal =
+        products[index].price * products[index].quantity;
+      setProductList(products);
+      setLastUpdatedIndex(index);
+    }
+  };
+
+  const TableDesktop = () => (
+    <table>
+      <thead>
+        <tr>
+          <th className="product-title">Produto</th>
+          <th className="right">Valor unitário</th>
+          <th className="right">Quantidade</th>
+          <th className="right">Subtotal</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {productList.map((product, index) => {
+          return (
+            <tr>
+              <td className="product-item">
+                <img src={ItemImage} alt="Imagem do item" width="120px" />
+                <p className="product-text">{product.name}</p>
+              </td>
+              <td className="right">{formatValues(product.price)}</td>
+              <td className="right">
+                <QuantityInput
+                  quantity={product.quantity}
+                  index={index}
+                  onClickLess={onClickLess}
+                  onClickMore={onClickMore}
+                  onChangeQuantity={onChangeQuantity}
+                  lastUpdatedIndex={lastUpdatedIndex}
+                />
+              </td>
+              <td className="right">{formatValues(product.subtotal)}</td>
+              <td>
+                <img
+                  className="icon-remove"
+                  src={DeleteIcon}
+                  onClick={() => onClickRemoveItem(index)}
+                  alt="Excluir"
+                />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+
+  const TableMobile = () => {
+    return productList.map((product, index) => {
+      return (
+        <section className="mobile-row">
+          <img src={ItemImage} alt="Imagem do item" width="120px" />
+
+          <div className="column">
+            <div className="mobile-row">
+              <p className="product-text">{product.name}</p>
+              <div className="right">{formatValues(product.price)}</div>
+            </div>
+
+            <div className="mobile-row">
+              <QuantityInput
+                quantity={product.quantity}
+                index={index}
+                onClickLess={onClickLess}
+                onClickMore={onClickMore}
+                onChangeQuantity={onChangeQuantity}
+                lastUpdatedIndex={lastUpdatedIndex}
+              />
+
+              <div className="right">{formatValues(product.subtotal)}</div>
+            </div>
+          </div>
+
+          <img
+            className="icon-remove"
+            src={DeleteIcon}
+            onClick={() => onClickRemoveItem(index)}
+            alt="Excluir"
+          />
+        </section>
+      );
+    });
   };
 
   return (
-    <main className="item-list-main">
+    <main
+      className="item-list-main"
+      style={{ width: isMobile ? "95vw" : "90vw" }}
+    >
       <h1 className="item-list-title">Carrinho</h1>
-      <section>
-        <table>
-          <thead>
-            <tr>
-              <th className="product-title">Produto</th>
-              <th className="value-title">Valor unitário</th>
-              <th className="quantity-titke">Quantidade</th>
-              <th className="subtotal-title">Subtotal</th>
-            </tr>
-          </thead>
+      <section>{isMobile ? <TableMobile /> : <TableDesktop />}</section>
 
-          <tbody>
-            {productList.map((product, index) => {
-              return (
-                <tr key={product.id}>
-                  <td className="product-item">
-                    <img
-                      src={product.image}
-                      alt="Imagem do item"
-                      width="120px"
-                    />
-                    <p>{product.name}</p>
-                  </td>
-                  <td className="right">{formatValues(product.value)}</td>
-                  <td className="right">
-                    <div className="td-quantify">
-                      <div
-                        className="btn-quantify less"
-                        onClick={() => onClickLess(index)}
-                      >
-                        -
-                      </div>
-                      {/* <div className="btn-quantifyList">{product.quantity}</div> */}
-                      <input
-                        type="number"
-                        value={product.quantity}
-                        onChange={(e) =>
-                          onChangeQuantity(index, e.target.value)
-                        }
-                      />
-                      <div
-                        className="btn-quantify more"
-                        onClick={() => onClickMore(index)}
-                      >
-                        +
-                      </div>
-                    </div>
-                  </td>
-                  <td className="right">{formatValues(product.subtotal)}</td>
-                  <td className="delete-icon-container">
-                    <img
-                      onClick={onClickRemoveItem}
-                      src={deleteIcon}
-                      alt="Excluir"
-                    />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </section>
-
-      <section className="clear-cart" onClick={removeAllItems}>
-        <div className="clear-cart-text">
-          <h3>Limpar carrinho</h3>
-        </div>
-        <div className="clear-cart-icon">
-          <img src={deleteIcon} alt="Excluir" width="30px" />
+      <section className="clear-cart-container">
+        <div className="clear-cart" onClick={removeAllItems}>
+          <div className="clear-cart-text">
+            <h3>Limpar carrinho</h3>
+          </div>
+          <div className="clear-cart-icon">
+            <img src={DeleteIcon} alt="Excluir" width="30px" />
+          </div>
         </div>
       </section>
 
-      <section className="total-price-container">
-        <section className="total-price">
-          <h3>TOTAL</h3>
-          <h3>{formatValues(calculateTotal())}</h3>
-        </section>
+      <section className="total-price">
+        <h3>TOTAL</h3>
+        <h3>{formatValues(calculateTotal())}</h3>
+      </section>
 
-        <section className="buy">
-          <div finish-info>
-            <button className="finish">FINALIZAR COMPRA</button>
-            <p className="btn-info">
-              Você será direcionado para o nosso <br /> WhatsApp para finalizar
-              seu pedido!
-            </p>
-          </div>
-          <div>
-            <button className="continue">CONTINUAR COMPRANDO</button>
-          </div>
-        </section>
+      <section className="buy">
+        <div finish-info>
+          <button className="finish">FINALIZAR COMPRA</button>
+          <p className="btn-info">
+            Você será direcionado para o nosso <br /> WhatsApp para finalizar
+            seu pedido!
+          </p>
+        </div>
+        <div>
+          <button className="continue">CONTINUAR COMPRANDO</button>
+        </div>
       </section>
     </main>
   );
---
+++ web/src/Components/ItemList/QuantityInput.jsx
@@ -0,0 +1,38 @@
+import { useRef, useEffect } from "react";
+
+const QuantityInput = ({
+  quantity,
+  index,
+  onClickLess,
+  onClickMore,
+  onChangeQuantity,
+  lastUpdatedIndex,
+}) => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (lastUpdatedIndex === index) {
+      console.log(">>> index", index);
+      inputRef.current.focus();
+    }
+  }, [lastUpdatedIndex, index, inputRef]);
+
+  return (
+    <div className="td-quantify">
+      <div className="btn-quantify less" onClick={() => onClickLess(index)}>
+        -
+      </div>
+      <input
+        type="number"
+        value={quantity}
+        onChange={(e) => onChangeQuantity(index, e)}
+        ref={inputRef}
+      />
+      <div className="btn-quantify more" onClick={() => onClickMore(index)}>
+        +
+      </div>
+    </div>
+  );
+};
+
+export default QuantityInput;
