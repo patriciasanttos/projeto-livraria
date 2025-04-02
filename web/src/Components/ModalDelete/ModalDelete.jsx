5e35bdaa61b6307860ef0f78ba751bd18f1aa83b
Patricia Santos <patriciasanttos.dev@gmail.com>
2025-04-02 20:38:55 -0300

    Responsive delete modal

--- web/src/Components/ModalDelete/ModalDelete.jsx
+++ web/src/Components/ModalDelete/ModalDelete.jsx
@@ -4,8 +4,10 @@ function ModalDelete({ setModalDelete, handleDelete }) {
   return (
     <section className="modal-delete">
       <div className="modal-delete-content">
-        <h2>Excluir item</h2>
-        <p>Você tem certeza que deseja excluir este item?</p>
+        <h2>Confirmação</h2>
+        <p>
+          Você tem certeza que deseja excluir <b>todos</b> os itens?
+        </p>
         <div className="modal-delete-buttons">
           <button className="delete" onClick={handleDelete}>
             Excluir
