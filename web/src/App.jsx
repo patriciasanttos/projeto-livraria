42ea0a34a4d42bcc2634ef31a929e28a9ddb1aa6
Guilherme Moreno <guilhermemorenoso@gmail.com>
2025-04-03 19:03:46 -0300

    Página About us

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,13 @@
+import AboutUs from './Pages/public/AboutUs/AboutUs';
 import MainRoutes from './routes';
 
 function App() {
   return <MainRoutes />
+    
+       
+       
+ 
+  
 }
 
 export default App
