a0b7593b66d01bfa0d497db816fe77d948486183
hector59a2b9297 <hector.02220x@gmail.com>
2025-04-03 23:36:44 -0300

    Feat: Home page refatored

--- web/src/Pages/public/HomePage/HomePage.jsx
+++ web/src/Pages/public/HomePage/HomePage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-import phoneImage from "../../../assets/Images/whatsapp-contact.svg"
 import mock from '../../../mocks/categoriesMocks.json'
-import './HomePage.scss';
+
+//-----Components
 import Category from '../../../components/Category/Category';
-import HomePageForm from '../../../Components/HomePageForm/HomePageForm';
+
+//-----Images
+import whatsappContactImage from '../../../assets/Images/whatsapp-contact.svg'
+import whatsappContactImageMobile from '../../../assets/Images/whatsapp-contact-mobile.svg'
+
+import './HomePage.scss';
 
 function HomePage() {
   const category1 = mock.data[0];
@@ -12,33 +17,39 @@ function HomePage() {
   const allCategories = mock.data;
 
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className='home-page'>
+      <section>
+        <Category
+          data={category1.items}
+          name={category1.name.charAt(0).toUpperCase() + category1.name.slice(1)}
+          categoryColor='pink'
+        />
 
-      <Category
-        data={category1.items}
-        name={category1.name.charAt(0).toUpperCase() + category1.name.slice(1)}
-        categoryColor='pink'
-      />
-      <Category
-        data={category2.items}
-        name={category2.name.charAt(0).toUpperCase() + category2.name.slice(1)}
-        categoryColor='green'
-      />
-      <Category
-        data={allCategories}
-        name='Compre por Categoria'
-        categoryColor='blue'
-        showOnlyCategories
-      />
+        <Category
+          data={category2.items}
+          name={category2.name.charAt(0).toUpperCase() + category2.name.slice(1)}
+          categoryColor='green'
+        />
 
-     <div className='phone-image'>
-     <a href="https://wa.me/5512982294420">
-        <img src={phoneImage} alt="" />
+        <Category
+          data={allCategories}
+          name='Compre por Categoria'
+          categoryColor='blue'
+          showOnlyCategories
+        />
+      </section>
 
+      <a target='_blank' href="https://wa.me/5512982294420">
+        <img
+          className='whatsapp-contact-image'
+          src={
+            window.innerWidth >= 768
+              ? whatsappContactImage
+              : whatsappContactImageMobile
+          }
+          alt="Whatsapp"
+        />
       </a>
-     </div>
-      <HomePageForm />
     </div>
   );
 }
