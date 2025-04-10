82f9362b38d8034286494fbc7f027249ddf97547
Patricia Santos <patriciasanttos.dev@gmail.com>
2025-04-09 22:31:03 -0300

    props added

--- web/src/Components/SearchInputAdmin/SearchInputAdmin.jsx
+++ web/src/Components/SearchInputAdmin/SearchInputAdmin.jsx
@@ -1,12 +1,18 @@
-import './SearchInputAdmin.scss'
+import "./SearchInputAdmin.scss";
 
-function SearchInputAdmin({title, placeholder, className}) {
-    return (
-        <section className='search-input-admin-container'>
-            <h3>{title}</h3>
-            <input type="text" placeholder={placeholder} className={className}/>
-        </section>
-    )
+function SearchInputAdmin({ title, placeholder, className, onChange, value }) {
+  return (
+    <section className="search-input-admin-container">
+      <h3>{title}</h3>
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={className}
+        onChange={onChange}
+        value={value}
+      />
+    </section>
+  );
 }
 
-export default SearchInputAdmin
+export default SearchInputAdmin;
