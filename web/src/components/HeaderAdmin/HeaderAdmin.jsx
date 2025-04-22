6208c385a46c4aeb0975926085e0e4af7c4c1e99
Patricia Santos <patriciasanttos.dev@gmail.com>
2025-04-22 00:43:01 -0300

    logout added to admin header

--- web/src/components/HeaderAdmin/HeaderAdmin.jsx
+++ web/src/components/HeaderAdmin/HeaderAdmin.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './HeaderAdmin.scss';
 
+import LogoutIcon from '../../assets/icons/logOut.svg'
+
 function HeaderAdmin({ page }) {
+
+    const onClickLogout = () => {
+        alert('coringamos')
+    }
+
     return (
-        <div className="admin-header">
-            <h2>{page}</h2>
+      <div className="admin-header">
+        <h2>{page}</h2>
+        <div className="logout-container" onClick={onClickLogout}>
+          <img src={LogoutIcon} alt="Ícone de sair" />
+          <p>Sair</p>
         </div>
+      </div>
     );
 }
 
