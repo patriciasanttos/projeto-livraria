c214db41db9d9f111231cb37bd5bc6a20cad4e2c
hector59a2b9297 <hector.02220x@gmail.com>
2025-04-22 01:58:32 -0300

    Fix: Build command changed

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from "@nestjs/common";
 import initSwagger from "./swagger";
 import * as cookieParser from "cookie-parser";
 
-export default async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
