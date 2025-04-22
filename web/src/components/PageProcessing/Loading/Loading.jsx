f1c506366929da2da74938873feab1a14142e9b5
Patricia Santos <patriciasanttos.dev@gmail.com>
2025-04-22 00:20:49 -0300

    hotfix: loading size adjusted for mobile

--- web/src/components/PageProcessing/Loading/Loading.jsx
+++ web/src/components/PageProcessing/Loading/Loading.jsx
@@ -4,7 +4,7 @@ function Loading({title, ...props}) {
     return (
       <section className='loading-section'>
         <div className="loader" {...props}></div>
-        <h1>
+        <h1 className='loading-title'>
             {title}
         </h1>
       </section>
