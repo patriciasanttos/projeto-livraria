b8cff4e27aeaef924d9e279f86fa341856a545e5
Patricia Santos <patriciasanttos.dev@gmail.com>
2025-04-22 12:04:32 -0300

    hotfix: google maps link added

--- web/src/components/Footer/FooterMobile/FooterMobile.jsx
+++ web/src/components/Footer/FooterMobile/FooterMobile.jsx
@@ -12,78 +12,75 @@ import { Link } from "react-router-dom";
 
 const FooterMobile = () => {
     return (
-        <footer className="footer-mobile">
-            <div className="first-footer-section">
-                <Link to="/">
-                    <img className="logo-footer" src={logo} alt="logo" />
-                </Link>
+      <footer className="footer-mobile">
+        <div className="first-footer-section">
+          <Link to="/">
+            <img className="logo-footer" src={logo} alt="logo" />
+          </Link>
 
-                <div>
-                    <div className="about">
-                        <Link to="/aboutus">
-                            <h2 className="flex-field-title">Sobre Nós</h2>
-                        </Link>
+          <div>
+            <div className="about">
+              <Link to="/aboutus">
+                <h2 className="flex-field-title">Sobre Nós</h2>
+              </Link>
 
-                        <h2 className="flex-field-title">Redes Sociais</h2>
+              <h2 className="flex-field-title">Redes Sociais</h2>
 
-                        <a className="social-media" href={import.meta.env.VITE_INSTAGRAM_URL} target="_blank">
-                            <img src={instagram} alt="insta-logo" />
-                            <p>@ciadaeducacaosjc</p>
-                        </a>
-                    </div>
-
-                    <div className="category">
-                        <h2 className="flex-field-title">Categorias</h2>
-
-                        <div>
-                            <Link to='/categories/presentes'>
-                                Presentes
-                            </Link>
-                            <Link to='/categories/livros%20infantis'>
-                                Livros Infantis
-                            </Link>
-                            <Link to='/categories/canetas'>
-                                Canetas
-                            </Link>
-                            <Link to='/categories'>
-                                Ver tudo
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+              <a
+                className="social-media"
+                href={import.meta.env.VITE_INSTAGRAM_URL}
+                target="_blank"
+              >
+                <img src={instagram} alt="insta-logo" />
+                <p>@ciadaeducacaosjc</p>
+              </a>
             </div>
 
-            <div className="time">
-                <div className="business-hour">
-                    <h3 className="flex-field-title">Horário de Funcionamento</h3>
+            <div className="category">
+              <h2 className="flex-field-title">Categorias</h2>
 
-                    <div>
-                        <p>
-                            Das 10h às 22h<br />
-                            Segunda a Sábado
-                        </p>
-                    </div>
-                </div>
-
-                <div className="card-flags">
-                    <h2 className="flex-field-title">Formas de Pagamento</h2>
-                    <img src={flags} alt="flags" className="flags" />
-                </div>
+              <div>
+                <Link to="/categories/presentes">Presentes</Link>
+                <Link to="/categories/livros%20infantis">Livros Infantis</Link>
+                <Link to="/categories/canetas">Canetas</Link>
+                <Link to="/categories">Ver tudo</Link>
+              </div>
             </div>
+          </div>
+        </div>
 
-            <div className="location">
-                <div>
-                    <img src={location} alt="location" className="location-img" />
-                    <h2 className="flex-field-title">Como Chegar</h2>
-                </div>
+        <div className="time">
+          <div className="business-hour">
+            <h3 className="flex-field-title">Horário de Funcionamento</h3>
 
-                <p>
-                    Shopping Jardim Oriente, <br />
-                    R. Andorra, 500 - Jardim America, <br />
-                    São José dos Campos - SP
-                </p>
+            <div>
+              <p>
+                Das 10h às 22h
+                <br />
+                Segunda a Sábado
+              </p>
             </div>
-        </footer>
+          </div>
+
+          <div className="card-flags">
+            <h2 className="flex-field-title">Formas de Pagamento</h2>
+            <img src={flags} alt="flags" className="flags" />
+          </div>
+        </div>
+
+        <div className="location">
+          <div>
+            <img src={location} alt="location" className="location-img" />
+            <h2 className="flex-field-title">Como Chegar</h2>
+          </div>
+
+          <a href="https://maps.app.goo.gl/KQMV5vfECNFFPpeF9" target="_blank">
+            Shopping Jardim Oriente, <br />
+            R. Andorra, 500 - Jardim America, <br />
+            São José dos Campos - SP
+          </a>
+        </div>
+      </footer>
     );
 };
 
