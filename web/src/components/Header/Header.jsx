ef47dd67f42c72084701d8e40887f8adaab6e57d
Patricia Santos <patty.renan.dev@gmail.com>
2026-04-03 20:19:13 -0300

    Responsive design header

--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import HeaderMobile from "./HeaderMobile/HeaderMobile";
 import HeaderDesktop from "./HeaderDesktop/HeaderDesktop";
+import useWindowWidth from "../../hooks/useWindowWidth";
 
 const Header = () => {
-  return window.innerWidth <= 821
+  const width = useWindowWidth();
+
+  return width <= 931
     ? <HeaderMobile />
     : <HeaderDesktop />;
 };
